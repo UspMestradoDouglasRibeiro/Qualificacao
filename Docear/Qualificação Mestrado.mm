cd475d86fa410f796f22542aadccde57f9139257
NB-ALIENWARE\Douglas <douglas.ribeiro@ig.com.br>
2016-08-22 18:33:56 -0300

    Inicio

---
+++ Docear/Qualificação Mestrado.mm
@@ -0,0 +1,89 @@
+<map version="docear 1.1" dcr_id="1471900359309_d7v6ukfz7vjkb0vw069gb3xb9" project="156B41917CE2LJLLA0S7D0NOPEB2TV6W47DV" project_last_home="file:/C:/Users/Douglas/Docear/projects/QualificacaoMestradoUSP">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Qualifica&#xe7;&#xe3;o Mestrado" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1471900502200" BACKGROUND_COLOR="#99ffff"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Resumo" POSITION="right" ID="ID_1462461510" CREATED="1471900380637" MODIFIED="1471901290409" VSHIFT="-10">
+<edge COLOR="#ff0000"/>
+<node TEXT="Portugu&#xea;s" ID="ID_789365017" CREATED="1471900398705" MODIFIED="1471900409860"/>
+<node TEXT="Ingl&#xea;s" ID="ID_1023954033" CREATED="1471900414080" MODIFIED="1471900418871"/>
+</node>
+<node TEXT="Introdu&#xe7;&#xe3;o" POSITION="left" ID="ID_565405555" CREATED="1471900891868" MODIFIED="1471901286353" HGAP="30" VSHIFT="-50">
+<edge COLOR="#0000ff"/>
+<node TEXT="Contextualiza&#xe7;&#xe3;o" ID="ID_961577066" CREATED="1471900901648" MODIFIED="1471900909448"/>
+<node TEXT="Motiva&#xe7;&#xe3;o" ID="ID_1690491922" CREATED="1471900912797" MODIFIED="1471900919298"/>
+<node TEXT="Objetivos" ID="ID_658490480" CREATED="1471900924412" MODIFIED="1471900928735"/>
+<node TEXT="Estrutura Proposta" FOLDED="true" ID="ID_1652812464" CREATED="1471900931360" MODIFIED="1471900940922">
+<node TEXT="" ID="ID_1458620656" CREATED="1471900944400" MODIFIED="1471900944400"/>
+</node>
+</node>
+<node TEXT="Fundament&#xe3;o Te&#xf3;rica" POSITION="right" ID="ID_645720407" CREATED="1471900959052" MODIFIED="1471901292580" VSHIFT="30">
+<edge COLOR="#00ff00"/>
+<node TEXT="Web Semantica" ID="ID_1523337139" CREATED="1471901124619" MODIFIED="1471901138472"/>
+<node TEXT="Linked Data" ID="ID_396695161" CREATED="1471901192595" MODIFIED="1471901239519" MOVED="1471901248879"/>
+<node TEXT="Ontologia" ID="ID_237534621" CREATED="1471901180870" MODIFIED="1471901184521" MOVED="1471901187086"/>
+<node TEXT="Resource Description Framework" ID="ID_1472313901" CREATED="1471901142454" MODIFIED="1471901153429"/>
+<node TEXT="Web Ontology Language" ID="ID_1033474448" CREATED="1471901156969" MODIFIED="1471901174999"/>
+</node>
+<node TEXT="Trabalhos Relacionados" POSITION="left" ID="ID_808007692" CREATED="1471901260307" MODIFIED="1471901321715" HGAP="30" VSHIFT="50">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Prot&#xe9;g&#xe9;" ID="ID_1958217059" CREATED="1471901323391" MODIFIED="1471901329769"/>
+<node TEXT="WebProt&#xe9;g&#xe9;" FOLDED="true" ID="ID_1783205320" CREATED="1471901332811" MODIFIED="1471901337135">
+<node TEXT="" ID="ID_1339918151" CREATED="1471901338188" MODIFIED="1471901338188"/>
+</node>
+<node TEXT="OWLViz" ID="ID_1250635242" CREATED="1471901342402" MODIFIED="1471901373160"/>
+<node TEXT="WebVOWL" ID="ID_445669261" CREATED="1471901351986" MODIFIED="1471901365827"/>
+</node>
+<node TEXT="Proposta de Trabalho" POSITION="right" ID="ID_140048812" CREATED="1471901295134" MODIFIED="1471901317929" HGAP="-10" VSHIFT="70">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
