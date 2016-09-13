67ca5f620c490ca4ba93ba6dd11c1ba2b65503c8
NB-ALIENWARE\Douglas <douglas.ribeiro@ig.com.br>
2016-09-13 00:12:42 -0300

    Atualização da Versão para Lyx. Correção de erros

--- Docear/Qualificação Mestrado.mm
+++ Docear/Qualificação Mestrado.mm
@@ -1,6 +1,8 @@
 <map version="docear 1.1" dcr_id="1471900359309_d7v6ukfz7vjkb0vw069gb3xb9" project="156B41917CE2LJLLA0S7D0NOPEB2TV6W47DV" project_last_home="file:/C:/Users/Douglas/Docear/projects/QualificacaoMestradoUSP">
 <!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
-<node TEXT="Qualifica&#xe7;&#xe3;o Mestrado" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1471900502200" BACKGROUND_COLOR="#99ffff"><hook NAME="MapStyle">
+<node TEXT="Qualifica&#xe7;&#xe3;o Mestrado" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1472728290898" BACKGROUND_COLOR="#99ffff">
+<font BOLD="true"/>
+<hook NAME="MapStyle">
     <properties show_note_icons="true"/>
 
 <map_styles>
@@ -51,74 +53,110 @@
 </map_styles>
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="7"/>
-<node TEXT="Resumo" POSITION="right" ID="ID_1462461510" CREATED="1471900380637" MODIFIED="1471901290409" VSHIFT="-10">
+<node TEXT="Resumo" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_1462461510" CREATED="1471900380637" MODIFIED="1472729664447" HGAP="270" VSHIFT="-220">
 <edge COLOR="#ff0000"/>
-<node TEXT="Portugu&#xea;s" ID="ID_789365017" CREATED="1471900398705" MODIFIED="1471900409860"/>
-<node TEXT="Ingl&#xea;s" ID="ID_1023954033" CREATED="1471900414080" MODIFIED="1471900418871"/>
+<node TEXT="Portugu&#xea;s" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_789365017" CREATED="1471900398705" MODIFIED="1472729849241">
+<node TEXT="Resumo de todo a Disserta&#xe7;&#xe3;o de&#xa;Qualifica&#xe7;&#xe3;o a ser apresentada" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_919899065" CREATED="1472727161316" MODIFIED="1472729842779"/>
 </node>
-<node TEXT="Introdu&#xe7;&#xe3;o" POSITION="left" ID="ID_565405555" CREATED="1471900891868" MODIFIED="1471901286353" HGAP="30" VSHIFT="-50">
+<node TEXT="Ingl&#xea;s" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1023954033" CREATED="1471900414080" MODIFIED="1472729849225">
+<node TEXT="Summary of all the Qualification&#xa;Dissertation to be presented" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1748328097" CREATED="1472727202643" MODIFIED="1472729842763"/>
+</node>
+</node>
+<node TEXT="Introdu&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="styles.topic" POSITION="left" ID="ID_565405555" CREATED="1471900891868" MODIFIED="1472729658333" HGAP="-470" VSHIFT="-130" MOVED="1472676193245">
 <edge COLOR="#0000ff"/>
-<node TEXT="Contextualiza&#xe7;&#xe3;o" ID="ID_961577066" CREATED="1471900901648" MODIFIED="1471900909448"/>
-<node TEXT="Motiva&#xe7;&#xe3;o" ID="ID_1690491922" CREATED="1471900912797" MODIFIED="1471900919298"/>
-<node TEXT="Objetivos" ID="ID_658490480" CREATED="1471900924412" MODIFIED="1471900928735"/>
-<node TEXT="Estrutura Proposta" FOLDED="true" ID="ID_1652812464" CREATED="1471900931360" MODIFIED="1471900940922">
-<node TEXT="" ID="ID_1458620656" CREATED="1471900944400" MODIFIED="1471900944400"/>
+<node TEXT="Contextualiza&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_961577066" CREATED="1471900901648" MODIFIED="1472729829122" HGAP="-230" VSHIFT="-10">
+<node TEXT="Uma breve introdu&#xe7;&#xe3;o sobre a Fundamenta&#xe7;&#xe3;o e&#xa;Ferramentas que utilizarei" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_362307170" CREATED="1472727285978" MODIFIED="1472729842748" HGAP="-410"/>
 </node>
-<node TEXT="Contribui&#xe7;&#xe3;o" ID="ID_1979250315" CREATED="1472636056892" MODIFIED="1472636715339"/>
+<node TEXT="Motiva&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1690491922" CREATED="1471900912797" MODIFIED="1472729829138" HGAP="-190" VSHIFT="-10">
+<node TEXT="N&#xe3;o existe um Editor Visual de Ontologias especifico&#xa;para este sistema SustenAgro e Nano da Embrapa" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_531438249" CREATED="1472727334472" MODIFIED="1472729842748" HGAP="-436"/>
 </node>
-<node TEXT="Fundament&#xe3;o Te&#xf3;rica" POSITION="right" ID="ID_645720407" CREATED="1471900959052" MODIFIED="1471901292580" VSHIFT="30">
+<node TEXT="Objetivos" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_658490480" CREATED="1471900924412" MODIFIED="1472729829106" HGAP="-180" VSHIFT="-10">
+<node TEXT="Desenvolver um Editor de Ontologias para Web, que&#xa;ser&#xe1; utilizado no sistema SustenAgro e Nano da Embrapa" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_342393414" CREATED="1472727414820" MODIFIED="1472729842743" HGAP="-470"/>
+</node>
+<node TEXT="Estrutura Proposta" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1652812464" CREATED="1471900931360" MODIFIED="1472729829106" HGAP="-230">
+<node TEXT="Indica&#xe7;&#xe3;o dos capitulos que falarei sobre Fundamenta&#xe7;&#xe3;o,&#xa;Desenvolvimento, Trabalhos Relacionados, etc" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1458620656" CREATED="1471900944400" MODIFIED="1472729842725" HGAP="-480"/>
+</node>
+<node TEXT="Contribui&#xe7;&#xe3;o" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1979250315" CREATED="1472636056892" MODIFIED="1472729829087" HGAP="-200" VSHIFT="20">
+<node TEXT="Demonstrar a utiliza&#xe7;&#xe3;o de um Editor de Ontologias de forma&#xa;Visual e para a Web, como ferramenta de Apoio a Sistemas Inteligentes,&#xa;sendo testado no Sistemas SustenAgro e Nano da Embrapa." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_650695070" CREATED="1472727611190" MODIFIED="1472729842710" HGAP="-560" VSHIFT="20"/>
+</node>
+</node>
+<node TEXT="Fundament&#xe3;o Te&#xf3;rica" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_645720407" CREATED="1471900959052" MODIFIED="1472729647563" HGAP="190" VSHIFT="170" MOVED="1472676193249">
 <edge COLOR="#00ff00"/>
-<node TEXT="Web Semantica" ID="ID_1523337139" CREATED="1471901124619" MODIFIED="1471911197392"><richcontent TYPE="NOTE">
-
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      teste de web semantica para ver o que faz !!!!
-    </p>
-  </body>
-</html>
-</richcontent>
+<node TEXT="Web Semantica" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1523337139" CREATED="1471901124619" MODIFIED="1472729803890">
+<node TEXT="Descrever a defini&#xe7;&#xe3;o de Web Semantica, suas funcionalidades,&#xa;e seus padr&#xf5;es" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1014968438" CREATED="1472727739783" MODIFIED="1472729793127" VSHIFT="-30"/>
 </node>
-<node TEXT="Linked Data" ID="ID_396695161" CREATED="1471901192595" MODIFIED="1471901239519" MOVED="1471901248879"/>
-<node TEXT="Ontologia" ID="ID_237534621" CREATED="1471901180870" MODIFIED="1471901184521" MOVED="1471901187086"/>
-<node TEXT="Resource Description Framework" ID="ID_1472313901" CREATED="1471901142454" MODIFIED="1471901153429"/>
-<node TEXT="Web Ontology Language" ID="ID_1033474448" CREATED="1471901156969" MODIFIED="1471901174999"/>
-<node TEXT="Embrapa" ID="ID_1074096855" CREATED="1472636777296" MODIFIED="1472636781984">
-<node TEXT="SustenAgro" ID="ID_1462834153" CREATED="1472636739625" MODIFIED="1472636786105" MOVED="1472636792175"/>
-<node TEXT="Nanotec" ID="ID_477299780" CREATED="1472636754621" MODIFIED="1472636765823" MOVED="1472636794701"/>
+<node TEXT="Linked Data" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_396695161" CREATED="1471901192595" MODIFIED="1472729803905" MOVED="1471901248879">
+<node TEXT="Descrever a defini&#xe7;&#xe3;o de Linked Data, e como podemos utiliza-lo." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1363190776" CREATED="1472727870731" MODIFIED="1472729793147"/>
+</node>
+<node TEXT="Ontologia" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_237534621" CREATED="1471901180870" MODIFIED="1472729803921" MOVED="1471901187086">
+<node TEXT="Descrever a defini&#xe7;&#xe3;o de Ontologia, Conceito e uso." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_643549965" CREATED="1472727955168" MODIFIED="1472729793147"/>
+</node>
+<node TEXT="Resource Description Framework Schema" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1472313901" CREATED="1471901142454" MODIFIED="1472729803921">
+<node TEXT="Introdu&#xe7;&#xe3;o ao conceito, formas de uso e suas vers&#xf5;es." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_576928402" CREATED="1472728000648" MODIFIED="1472729793147"/>
+</node>
+<node TEXT="Web Ontology Language" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1033474448" CREATED="1471901156969" MODIFIED="1472729803921">
+<node TEXT="Defini&#xe7;&#xe3;o, uso e diferen&#xe7;a entre RDFs e OWL" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_910430276" CREATED="1472728073336" MODIFIED="1472729793163"/>
+</node>
+<node TEXT="Embrapa" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1074096855" CREATED="1472636777296" MODIFIED="1472729803937" VSHIFT="10">
+<node TEXT="SustenAgro" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_1462834153" CREATED="1472636739625" MODIFIED="1472729808628" MOVED="1472636792175">
+<node TEXT="Apresentar o SustenAgro, seus conceitos e uso" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1326822669" CREATED="1472728252493" MODIFIED="1472729793163"/>
+</node>
+<node TEXT="Nanotec" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_477299780" CREATED="1472636754621" MODIFIED="1472729818136" MOVED="1472636794701" VSHIFT="10">
+<node TEXT="Apresentar o Nanotec, seus conceitos e uso" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1316073525" CREATED="1472728274881" MODIFIED="1472729793163"/>
 </node>
 </node>
-<node TEXT="Trabalhos Relacionados" POSITION="left" ID="ID_808007692" CREATED="1471901260307" MODIFIED="1471901321715" HGAP="30" VSHIFT="50">
+</node>
+<node TEXT="Trabalhos Relacionados" LOCALIZED_STYLE_REF="styles.topic" POSITION="left" ID="ID_808007692" CREATED="1471901260307" MODIFIED="1472729640550" HGAP="-490" VSHIFT="190">
 <edge COLOR="#ff00ff"/>
-<node TEXT="Prot&#xe9;g&#xe9;" ID="ID_1958217059" CREATED="1471901323391" MODIFIED="1471901329769"/>
-<node TEXT="WebProt&#xe9;g&#xe9;" ID="ID_1783205320" CREATED="1471901332811" MODIFIED="1471901337135">
-<node TEXT="Introdu&#xe7;&#xe3;o" ID="ID_1339918151" CREATED="1471901338188" MODIFIED="1471902580728"/>
-<node TEXT="WEBPROTEGE: SUPPORTING THE CREATION OF ICD-11" ID="ID_284246260" CREATED="1471902586822" MODIFIED="1471902657560" LINK="file:/C:/Users/Douglas/OneDrive/Documentos/ICMC_USP/Mestrado/Qualifica&#xe7;&#xe3;o/Docear/http:/iswc2010.semanticweb.org/accepted-poster-demo/502.html"/>
+<node TEXT="Prot&#xe9;g&#xe9;" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1958217059" CREATED="1471901323391" MODIFIED="1472729769421" HGAP="-230">
+<node TEXT="Defini&#xe7;&#xe3;o do Prot&#xe9;g&#xe9;, suas caracteristicas e as caracteristicas similares&#xa;ou n&#xe3;o com a proposta deste trabalho" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1017808995" CREATED="1472728508449" MODIFIED="1472729760824" HGAP="-578" VSHIFT="-10"/>
 </node>
-<node TEXT="OWLViz" ID="ID_1250635242" CREATED="1471901342402" MODIFIED="1471901373160"/>
-<node TEXT="WebVOWL" ID="ID_445669261" CREATED="1471901351986" MODIFIED="1471902711471" LINK="file:/C:/Users/Douglas/OneDrive/Documentos/http:/iswc2010.semanticweb.org/accepted-poster-demo/502.html"/>
+<node TEXT="WebProt&#xe9;g&#xe9;" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1783205320" CREATED="1471901332811" MODIFIED="1472729769436" HGAP="-250" VSHIFT="10">
+<node TEXT="Defini&#xe7;&#xe3;o do WebProt&#xe9;g&#xe9;, suas caracteristicas e as caracteristicas similares&#xa;ou n&#xe3;o com a proposta deste trabalho" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1339918151" CREATED="1471901338188" MODIFIED="1472729760841" HGAP="-610" VSHIFT="-20"/>
+<node TEXT="WEBPROTEGE: SUPPORTING THE CREATION OF ICD-11" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_284246260" CREATED="1471902586822" MODIFIED="1472729929991" LINK="file:/C:/Users/Douglas/OneDrive/Documentos/ICMC_USP/Mestrado/Qualifica&#xe7;&#xe3;o/Docear/http:/iswc2010.semanticweb.org/accepted-poster-demo/502.html" HGAP="-493">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_284246260" STARTINCLINATION="40;0;" ENDINCLINATION="40;45;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Exemplo de uso do WebProt&#xe9;g&#xe9; para desenvolvimento&#xa;de um sistema especifico (Exemplo)" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_357096403" CREATED="1472728560879" MODIFIED="1472729760809" HGAP="-713" VSHIFT="-10"/>
 </node>
-<node TEXT="Proposta de Trabalho" POSITION="right" ID="ID_140048812" CREATED="1471901295134" MODIFIED="1471901317929" HGAP="-10" VSHIFT="70">
+</node>
+<node TEXT="OWLViz" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1250635242" CREATED="1471901342402" MODIFIED="1472729769452" HGAP="-230" VSHIFT="10">
+<node TEXT="Defini&#xe7;&#xe3;o do OWLViz, suas caracteristicas e as caracteristicas similares&#xa;ou n&#xe3;o com a proposta deste trabalho" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_241767011" CREATED="1472728650714" MODIFIED="1472729760809" HGAP="-560" VSHIFT="20"/>
+</node>
+<node TEXT="WebVOWL" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_445669261" CREATED="1471901351986" MODIFIED="1472729944407" LINK="file:/C:/Users/Douglas/OneDrive/Documentos/http:/iswc2010.semanticweb.org/accepted-poster-demo/502.html" HGAP="-260" VSHIFT="20">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_445669261" STARTINCLINATION="40;0;" ENDINCLINATION="40;45;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Defini&#xe7;&#xe3;o do WebVOWL, suas caracteristicas e caracteristicas similares&#xa;ou n&#xe3;o com a proposta deste trabalho" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1889644213" CREATED="1472728750189" MODIFIED="1472729760793" HGAP="-570" VSHIFT="10"/>
+</node>
+</node>
+<node TEXT="Proposta de Trabalho" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_140048812" CREATED="1471901295134" MODIFIED="1472729632786" HGAP="180" VSHIFT="360" MOVED="1472676193243">
 <edge COLOR="#00ffff"/>
-<node TEXT="Metodologia" ID="ID_472052103" CREATED="1472636888976" MODIFIED="1472636894570"/>
-<node TEXT="Sistema Web" ID="ID_1974699312" CREATED="1472636897507" MODIFIED="1472636911724"/>
-<node TEXT="Ferramentas Utilizadas" ID="ID_937739500" CREATED="1472636922427" MODIFIED="1472636989293">
-<node TEXT="Groovy/Grails" ID="ID_271801442" CREATED="1472637003743" MODIFIED="1472637300451"/>
-<node TEXT="Canvas e SVG" ID="ID_1084583555" CREATED="1472637030462" MODIFIED="1472637039944" MOVED="1472637032773"/>
-<node TEXT="Ontologias (TripleStores)" ID="ID_1198607062" CREATED="1472637237328" MODIFIED="1472637324743"/>
-<node TEXT="Angular 2" ID="ID_1630183181" CREATED="1472637286320" MODIFIED="1472637291416"/>
+<node TEXT="Metodologia" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_472052103" CREATED="1472636888976" MODIFIED="1472729709056">
+<node TEXT="Descrever a forma como este trabalho ser&#xe1; desenvolvido" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_973266896" CREATED="1472728873718" MODIFIED="1472729748009" HGAP="40"/>
 </node>
-<node TEXT="Desenvolvimento" ID="ID_612840132" CREATED="1472637052186" MODIFIED="1472637343063" VSHIFT="20">
-<node TEXT="Atividades Previstas e Cronogramas" ID="ID_666664128" CREATED="1472637062178" MODIFIED="1472637074625"/>
-<node TEXT="Atividades Conclu&#xed;das at&#xe9; o momento" ID="ID_86640138" CREATED="1472637091761" MODIFIED="1472637102908"/>
-<node TEXT="Dificuldades e Limita&#xe7;&#xf5;es" ID="ID_337434946" CREATED="1472637106370" MODIFIED="1472637168941"/>
+<node TEXT="Ferramentas Utilizadas" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_937739500" CREATED="1472636922427" MODIFIED="1472729712783">
+<node TEXT="Groovy/Grails" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_271801442" CREATED="1472637003743" MODIFIED="1472729730557">
+<node TEXT="Descrever as caracteristicas da linguagem, e o porque de&#xa;sua utiliza&#xe7;&#xe3;o neste projeto" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_731557450" CREATED="1472729055382" MODIFIED="1472729748017"/>
+</node>
+<node TEXT="Canvas e SVG" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_1084583555" CREATED="1472637030462" MODIFIED="1472729730555" MOVED="1472637032773" VSHIFT="10">
+<node TEXT="Descrever as caracteristicas deste padr&#xe3;o, e o porque de&#xa;sua utiliza&#xe7;&#xe3;o neste projeto" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_935104784" CREATED="1472729096096" MODIFIED="1472729748026"/>
+</node>
+<node TEXT="Ontologias (TripleStores)" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_1198607062" CREATED="1472637237328" MODIFIED="1472729730546" VSHIFT="10">
+<node TEXT="Descrever as caracteristcas, o porque de sua utiliza&#xe7;&#xe3;o&#xa;neste projeto e o uso para pequenas infer&#xea;ncias para&#xa;teste de validade da ontologia criada." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_974821291" CREATED="1472729184579" MODIFIED="1472729748035"/>
+</node>
+<node TEXT="Angular 2" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_1630183181" CREATED="1472637286320" MODIFIED="1472729730533" VSHIFT="10">
+<node TEXT="Descrever as caracteristicas deste padr&#xe3;o, e o porque de sua&#xa;utiliza&#xe7;&#xe3;o neste projeto" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_421184224" CREATED="1472729255032" MODIFIED="1472729748049"/>
 </node>
 </node>
-<node TEXT="Conclus&#xe3;o" POSITION="left" ID="ID_1336138408" CREATED="1472637186929" MODIFIED="1472637220120" HGAP="-20" VSHIFT="70">
+<node TEXT="Desenvolvimento" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_612840132" CREATED="1472637052186" MODIFIED="1472729716273" VSHIFT="20">
+<node TEXT="Atividades Previstas e Cronogramas" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_666664128" CREATED="1472637062178" MODIFIED="1472729722581">
+<node TEXT="Descrever as atividades a serem realizadas neste projeto e&#xa;suas devidas data para inicio e termino" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1631900145" CREATED="1472729301957" MODIFIED="1472729748044"/>
+</node>
+<node TEXT="Dificuldades e Limita&#xe7;&#xf5;es" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_337434946" CREATED="1472637106370" MODIFIED="1472729722581">
+<node TEXT="Expor as possiveis dificildades e limita&#xe7;&#xf5;es que poderei encontrar no&#xa;transcorrer da execu&#xe7;&#xe3;o deste projeto" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_724064011" CREATED="1472729341858" MODIFIED="1472729748049" HGAP="40" VSHIFT="20"/>
+</node>
+</node>
+</node>
+<node TEXT="Conclus&#xe3;o" LOCALIZED_STYLE_REF="styles.topic" POSITION="left" ID="ID_1336138408" CREATED="1472637186929" MODIFIED="1472729673117" HGAP="-440" VSHIFT="454" MOVED="1472676193250">
 <edge COLOR="#7c0000"/>
+<node TEXT="Descrever a conclus&#xe3;o do trabalho" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1901614973" CREATED="1472728175926" MODIFIED="1472729748049" HGAP="-300"/>
 </node>
 </node>
 </map>
